f822696e7b8c4b2562061d9c14766e05bdc8c21e
michaelsowka <michaelsznipe@gmail.com>
2024-04-16 23:34:54 +0200

    Added BonusHealth Interactable and generic UI

---
+++ Assets/Scripts/Managers/UIManager.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public TextMeshProUGUI hpText;
+    public TextMeshProUGUI crystalProgressText;
+    public TextMeshProUGUI crystalCarriedText;
+    public static UIManager Instance;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void UpdateHpText()
+    {
+        hpText.text = "" + CharacterManager.Instance.GetHp();
+    }
+
+    public void UpdateCrystalCarriedText()
+    {
+        crystalCarriedText.text = "" + CharacterManager.Instance.GetCrystalCarried();
+    }
+
+    public void UpdateCrystalsLeftText()
+    {
+        crystalProgressText.text =
+            "" + (LevelManager.Instance.GetCrystalsInLevel() - LevelManager.Instance.GetCrystalsLeft()) + "/" +
+            LevelManager.Instance.GetCrystalsInLevel();
+    }
+
+    /*
+     * This can not be done in the Start() function of the UIManager due to a race condition,
+     * so it needs to be invoked in the ChracterManager Start() function after all values have been
+     * initialized.
+     */
+    public void InitializeUI()
+    {
+        UpdateHpText();
+        UpdateCrystalCarriedText();
+        UpdateCrystalsLeftText();
+    }
+}
